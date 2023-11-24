d0fa7636d2712000ea82881daff2ed2ca834ea6e
wanda <rashiidahweira@gmail.com>
2023-11-24 13:22:30 +0300

    Clean up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,32 +3,34 @@ import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="md:pl-16">
+    <div className="w-full md:pl-16">
       <h1 className="font-semibold text-3xl">Projects</h1>
-      <div className="flex flex-col md:flex-row md:gap-36 mt-4">
-        <ProjectCard
-          image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
-          name="Project 1"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-        <ProjectCard
-          image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
-          name="Project 2"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-      </div>
+      <div className="mx-28">
+        <div className="flex flex-col md:flex-row md:gap-36 mt-4">
+          <ProjectCard
+            image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
+            name="Project 1"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+          <ProjectCard
+            image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
+            name="Project 2"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+        </div>
 
-      <div className="flex flex-col md:flex-row md:gap-36 mt-4">
-        <ProjectCard
-          image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
-          name="Project 3"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-        <ProjectCard
-          image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
-          name="Project 4"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
+        <div className="flex flex-col md:flex-row md:gap-36 mt-4">
+          <ProjectCard
+            image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
+            name="Project 3"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+          <ProjectCard
+            image="https://static3.depositphotos.com/1003681/164/i/600/depositphotos_1646367-stock-photo-crocus.jpg"
+            name="Project 4"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -11,13 +11,31 @@ const Technologies = () => {
             <p>Advanced</p>
           </div>
           <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
-            <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-1/4 rounded-xl"></div>
+            <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-3/4 rounded-xl"></div>
           </div>
         </div>
         <div className="w-full my-4">
           <div className="flex justify-between">
-            <p>HTML</p>
+            <p>CSS, SASS Bootstrap, Material Design, Material UI, Chakra, Tailwindcss</p>
             <p>Advanced</p>
+          </div>
+          <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
+            <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-3/4 rounded-xl"></div>
+          </div>
+        </div>
+        <div className="w-full my-4">
+          <div className="flex justify-between">
+            <p>Javascript, Typescript</p>
+            <p>Advanced</p>
+          </div>
+          <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
+            <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-3/4 rounded-xl"></div>
+          </div>
+        </div>
+        <div className="w-full my-4">
+          <div className="flex justify-between">
+            <p>Figma, UI/UX</p>
+            <p>Regular</p>
           </div>
           <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
             <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-1/2 rounded-xl"></div>
@@ -25,7 +43,25 @@ const Technologies = () => {
         </div>
         <div className="w-full my-4">
           <div className="flex justify-between">
-            <p>HTML</p>
+            <p>Angular</p>
+            <p>Beginner</p>
+          </div>
+          <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
+            <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-1/4 rounded-xl"></div>
+          </div>
+        </div>
+        <div className="w-full my-4">
+          <div className="flex justify-between">
+            <p>React</p>
+            <p>Advanced</p>
+          </div>
+          <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
+            <div className="absolute top-0 left-0 bg-gradient-to-r from-cyan-400 to-violet-900 h-full w-3/4 rounded-xl"></div>
+          </div>
+        </div>
+        <div className="w-full my-4">
+          <div className="flex justify-between">
+            <p>React Native</p>
             <p>Advanced</p>
           </div>
           <div className="relative pt-0.5 text-xs text-center text-white bg-lighter w-full rounded-xl h-4">
